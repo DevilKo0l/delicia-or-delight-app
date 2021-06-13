a8edc187a9f656648394d47641be8d616f992dc0
Devilko0l <nguyen.tcao96@gmail.com>
2021-06-14 00:26:34 +0200

    finish setup

--- App.js
+++ App.js
@@ -7,20 +7,19 @@ import { Home, Restaurant, OrderDelivery } from './screens'
 const Stack = createStackNavigator();
 
 const App = () => {
-
   return (
     <NavigationContainer>
       <Stack.Navigator
-        screenOptions = {{
+        screenOptions={{
           headerShown: false
         }}
         initialRouteName={"Home"}
       >
         <Stack.Screen name="Home" component={Home}/>
         <Stack.Screen name="Restaurant" component={Restaurant}/>
-        <Stack.Screen name="OrderDelivery" component={Home}/>
+        <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
 }
-
+export default App;
